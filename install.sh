7ef36d3b8aea630eb0a18af6c3d9c09a1c9fde9d
ArneBachmann <ArneBachmann@users.noreply.github.com>
2017-12-26 23:54:20 +0100

    CI

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 echo NOMYPY=$NOMYPY
 if [ "x$NOMYPY" == "x" ]
 then
-	pip install appdirs chardet configr wcwidth coverage python-coveralls coconut 
+	pip install --upgrade appdirs chardet configr wcwidth coverage python-coveralls coconut 
 else
-	pip install appdirs chardet configr wcwidth coverage python-coveralls coconut[mypy]
+	pip install --upgrade appdirs chardet configr wcwidth coverage python-coveralls coconut[mypy]
 fi
