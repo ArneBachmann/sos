463b9a81f706bb88c64763b3888d43b489848ad7
ArneBachmann <ArneBachmann@users.noreply.github.com>
2018-02-01 21:08:45 +0100

    A hunch

--- install.bat
+++ install.bat
@@ -4,7 +4,11 @@ echo NOMYPY=%NOMYPY%
 python -m pip install -U pip
 
 if "%NOMYPY%" == "" (
-	pip install -U appdirs chardet configr "setuptools >= 20.8.1" termwidth coverage python-coveralls coconut[mypy]
+    pip install -U appdirs chardet configr "setuptools >= 20.8.1" termwidth coverage python-coveralls coconut[mypy,cPyparsing]
 ) else (
-	pip install -U appdirs chardet configr "setuptools >= 20.8.1" termwidth coverage python-coveralls coconut
+    pip install -U appdirs chardet configr "setuptools >= 20.8.1" termwidth coverage python-coveralls coconut
+)
+
+if "%BACKPORT%" == "true" (
+    pip install -U enum34
 )
--- install.sh
+++ install.sh
@@ -4,7 +4,12 @@ echo NOMYPY=$NOMYPY
 python -m pip install -U pip
 if [ "x$NOMYPY" == "x" ]
 then
-	pip install -U appdirs chardet configr "setuptools >= 20.8.1" termwidth coverage python-coveralls coconut[mypy]
+    pip install -U appdirs chardet configr "setuptools >= 20.8.1" termwidth coverage python-coveralls coconut[mypy,cPyparsing]
 else
-	pip install -U appdirs chardet configr "setuptools >= 20.8.1" termwidth coverage python-coveralls coconut
+    pip install -U appdirs chardet configr "setuptools >= 20.8.1" termwidth coverage python-coveralls coconut
+fi
+
+if [ "x$BACKPORT" == "xtrue" ]
+then
+    pip install -U enum34
 fi
