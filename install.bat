a3cf02883bb4cf04165c8a8e37617d0fed3590ed
ArneBachmann <ArneBachmann@users.noreply.github.com>
2018-02-02 20:44:59 +0100

    Fixes #159

--- install.bat
+++ install.bat
@@ -6,7 +6,7 @@ python -m pip install -U pip
 if "%NOMYPY%" == "" (
     pip install -U appdirs chardet configr "setuptools >= 20.8.1" termwidth coverage python-coveralls coconut[mypy,cPyparsing]
 ) else (
-    pip install -U appdirs chardet configr "setuptools >= 20.8.1" termwidth coverage python-coveralls coconut
+    pip install -U appdirs chardet configr "setuptools >= 20.8.1" termwidth coverage python-coveralls coconut[cPyparsing]
 )
 
 if "%BACKPORT%" == "true" (
--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ if [ "x$NOMYPY" == "x" ]
 then
     pip install -U appdirs chardet configr "setuptools >= 20.8.1" termwidth coverage python-coveralls coconut[mypy,cPyparsing]
 else
-    pip install -U appdirs chardet configr "setuptools >= 20.8.1" termwidth coverage python-coveralls coconut
+    pip install -U appdirs chardet configr "setuptools >= 20.8.1" termwidth coverage python-coveralls coconut[cPyparsing]
 fi
 
 if [ "x$BACKPORT" == "xtrue" ]
