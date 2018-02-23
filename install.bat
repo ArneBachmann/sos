17045527ae013e5b95824011c92ade3d731f5a7d
ArneBachmann <ArneBachmann@users.noreply.github.com>
2018-02-23 10:56:24 +0100

    Small CI and typing improvements

--- install.bat
+++ install.bat
@@ -5,9 +5,9 @@ echo BACKPORT=%BACKPORT%
 python -m pip install -U pip
 
 if "%NOMYPY%" == "" (
-    pip install -U appdirs chardet configr "setuptools >= 20.8.1" termwidth coverage python-coveralls coconut[mypy,cPyparsing]
+    pip install -U appdirs chardet configr "setuptools >= 20.8.1" termwidth coverage python-coveralls "coconut-develop[mypy,cPyparsing] <= 1.3.1.post0.dev20"
 ) else (
-    pip install -U appdirs chardet configr "setuptools >= 20.8.1" termwidth coverage python-coveralls coconut[cPyparsing]
+    pip install -U appdirs chardet configr "setuptools >= 20.8.1" termwidth coverage python-coveralls "coconut[cPyparsing] <= 1.3.1.post0.dev20"
 )
 
 if "%BACKPORT%" == "true" (
