913e7218eed0d0fe3382dee37f06e36f510a71d9
ArneBachmann <ArneBachmann@users.noreply.github.com>
2018-02-23 13:06:43 +0100

    CI

--- install.bat
+++ install.bat
@@ -5,9 +5,9 @@ echo BACKPORT=%BACKPORT%
 python -m pip install -U pip
 
 if "%NOMYPY%" == "" (
-    pip install -U appdirs chardet configr "setuptools >= 20.8.1" termwidth coverage python-coveralls "coconut-develop[mypy,cPyparsing] <= 1.3.1.post0.dev20"
+    pip install -U appdirs chardet configr "setuptools >= 20.8.1" termwidth coverage python-coveralls coconut[mypy,cPyparsing]
 ) else (
-    pip install -U appdirs chardet configr "setuptools >= 20.8.1" termwidth coverage python-coveralls "coconut-develop[cPyparsing] <= 1.3.1.post0.dev20"
+    pip install -U appdirs chardet configr "setuptools >= 20.8.1" termwidth coverage python-coveralls coconut[cPyparsing]
 )
 
 if "%BACKPORT%" == "true" (
