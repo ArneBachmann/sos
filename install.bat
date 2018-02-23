71c2f449be121be80750e26bf082b5a52f6ca020
ArneBachmann <ArneBachmann@users.noreply.github.com>
2018-02-23 12:17:29 +0100

    CI

--- install.bat
+++ install.bat
@@ -7,7 +7,7 @@ python -m pip install -U pip
 if "%NOMYPY%" == "" (
     pip install -U appdirs chardet configr "setuptools >= 20.8.1" termwidth coverage python-coveralls "coconut-develop[mypy,cPyparsing] <= 1.3.1.post0.dev20"
 ) else (
-    pip install -U appdirs chardet configr "setuptools >= 20.8.1" termwidth coverage python-coveralls "coconut[cPyparsing] <= 1.3.1.post0.dev20"
+    pip install -U appdirs chardet configr "setuptools >= 20.8.1" termwidth coverage python-coveralls "coconut-develop[cPyparsing] <= 1.3.1.post0.dev20"
 )
 
 if "%BACKPORT%" == "true" (
--- install.sh
+++ install.sh
@@ -4,9 +4,9 @@ echo NOMYPY=$NOMYPY
 python -m pip install -U pip
 if [ "x$NOMYPY" == "x" ]
 then
-    pip install -U appdirs chardet configr "setuptools >= 20.8.1" termwidth coverage python-coveralls "coconut[mypy,cPyparsing] <= 1.3.1.post0.dev20"
+    pip install -U appdirs chardet configr "setuptools >= 20.8.1" termwidth coverage python-coveralls "coconut-develop[mypy,cPyparsing] <= 1.3.1.post0.dev20"
 else
-    pip install -U appdirs chardet configr "setuptools >= 20.8.1" termwidth coverage python-coveralls "coconut[cPyparsing] <= 1.3.1.post0.dev20"
+    pip install -U appdirs chardet configr "setuptools >= 20.8.1" termwidth coverage python-coveralls "coconut-develop[cPyparsing] <= 1.3.1.post0.dev20"
 fi
 
 if [ "x$BACKPORT" == "xtrue" ]
