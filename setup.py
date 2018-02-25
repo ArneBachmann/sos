2440ecd3c282a484ed612d7ec327b1c7d25dfaee
ArneBachmann <ArneBachmann@users.noreply.github.com>
2018-02-25 12:07:10 +0100

    Testing an issue that takes forever in build_ext

--- setup.py
+++ setup.py
@@ -4,19 +4,17 @@
 import os, shutil, subprocess, sys, time, unittest
 from setuptools import setup, find_packages
 
-RELEASE = "1.4.3"
+RELEASE = "1.4.4"
 
 print("sys.argv is %r" % sys.argv)
 readmeFile = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'README.md')
 if 'build' in sys.argv:
   print("Transpiling Coconut files to Python...")
-  cmd = "-develop" if 0 == subprocess.Popen("coconut-develop --help", shell = True, stdout = subprocess.PIPE, stderr = subprocess.PIPE, bufsize = 10000000).wait() and os.getenv("NODEV", "false").strip().lower() != "true" else ""
+  cmd = "-develop" if 0 == subprocess.Popen("coconut-develop --help", shell = True, stdout = subprocess.PIPE, stderr = subprocess.PIPE, bufsize = 1000000).wait() and os.getenv("NODEV", "false").strip().lower() != "true" else ""
 
-  assert 0 == os.system("coconut%s %s %s -l -t 3.4 sos %s" % (cmd, "-p" if not "--mypy" in sys.argv else "", "--force" if "--force" in sys.argv else "", "--mypy --ignore-missing-imports --warn-incomplete-stub --warn-redundant-casts --warn-unused-ignores" if "--mypy" in sys.argv else ""))  #  or useChanges
-  try: sys.argv.remove('--mypy')
-  except: pass
-  try: sys.argv.remove('--force')
-  except: pass
+  assert 0 == os.system("coconut%s %s %s -l -t 3.4 sos%s" % (cmd, "-p" if not "--mypy" in sys.argv else "", "--force" if "--force" in sys.argv else "", " --mypy --ignore-missing-imports --warn-incomplete-stub --warn-redundant-casts --warn-unused-ignores" if "--mypy" in sys.argv else ""))  #  or useChanges
+  if "--mypy" in sys.argv:  sys.argv.remove('--mypy')
+  if "--force" in sys.argv: sys.argv.remove('--force')
 
   if os.path.exists(".git"):
     print("Preparing documentation for PyPI by converting from Markdown to reStructuredText via pandoc")
@@ -106,7 +104,7 @@ setup(
   package_dir = {"sos": "sos"},
   package_data = {"": ["../LICENSE", "../README.md", "../README.rst", "*.coco"]},
   include_package_data = False,  # if True, will *NOT* package the data!
-  zip_safe = False,
+  zip_safe = False,  # TODO re-add and test extras section for backport = enum34
   entry_points = {
     'console_scripts': [
       'sos=sos.sos:main'  # Subversion offline solution
