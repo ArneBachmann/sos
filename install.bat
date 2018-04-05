43363969f5a4fa179337545e9111a24a45d5c30c
ArneBachmann <ArneBachmann@users.noreply.github.com>
2018-04-05 20:15:58 +0200

    Test coconut-develop one more time on CI

--- install.bat
+++ install.bat
@@ -5,9 +5,9 @@ echo BACKPORT=%BACKPORT%
 python -m pip install -U pip
 
 if "%NOMYPY%" == "" (
-    pip install -U appdirs chardet configr "setuptools >= 20.8.1" termwidth coverage python-coveralls coconut[mypy,cPyparsing]
+    pip install -U appdirs chardet configr "setuptools >= 20.8.1" termwidth coverage python-coveralls coconut-develop[mypy,cPyparsing]
 ) else (
-    pip install -U appdirs chardet configr "setuptools >= 20.8.1" termwidth coverage python-coveralls coconut[cPyparsing]
+    pip install -U appdirs chardet configr "setuptools >= 20.8.1" termwidth coverage python-coveralls coconut-develop[cPyparsing]
 )
 
 if "%BACKPORT%" == "true" (
--- install.sh
+++ install.sh
@@ -4,9 +4,9 @@ echo NOMYPY=$NOMYPY
 python -m pip install -U pip
 if [ "x$NOMYPY" == "x" ]
 then
-    pip install -U appdirs chardet configr "setuptools >= 20.8.1" termwidth coverage python-coveralls coconut[mypy,cPyparsing]
+    pip install -U appdirs chardet configr "setuptools >= 20.8.1" termwidth coverage python-coveralls coconut-develop[mypy,cPyparsing]
 else
-    pip install -U appdirs chardet configr "setuptools >= 20.8.1" termwidth coverage python-coveralls coconut[cPyparsing]
+    pip install -U appdirs chardet configr "setuptools >= 20.8.1" termwidth coverage python-coveralls coconut-develop[cPyparsing]
 fi
 
 if [ "x$BACKPORT" == "xtrue" ]
