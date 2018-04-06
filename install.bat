0539493a267ef7763f01eb673569d223b82e5efa
ArneBachmann <ArneBachmann@users.noreply.github.com>
2018-04-06 11:18:27 +0200

    Testing P3.3 again

--- install.bat
+++ install.bat
@@ -1,17 +1,12 @@
 
 echo NOMYPY=%NOMYPY%
-echo BACKPORT=%BACKPORT%
 
 python -m pip install -U pip
 
 if "%NOMYPY%" == "" (
-    pip install -U appdirs chardet configr termwidth coverage python-coveralls "typing >= '3.5' ; python_version < '3.5'"
+    pip install -U appdirs chardet configr termwidth coverage python-coveralls "typing >= '3.5' ; python_version < '3.5'" "enum34 ; python_version < '3.4'"
 	rem "setuptools >= 20.8.1" coconut-develop[mypy,cPyparsing]
 ) else (
-    pip install -U appdirs chardet configr  termwidth coverage python-coveralls "typing >= '3.5' ; python_version < '3.5'"
+    pip install -U appdirs chardet configr  termwidth coverage python-coveralls "typing >= '3.5' ; python_version < '3.5'" "enum34 ; python_version < '3.4'"
     rem "setuptools >= 20.8.1" coconut-develop[cPyparsing]
-)
-
-if "%BACKPORT%" == "true" (
-    pip install -U enum34
 )
--- install.sh
+++ install.sh
@@ -4,14 +4,9 @@ echo NOMYPY=$NOMYPY
 python -m pip install -U pip
 if [ "x$NOMYPY" == "x" ]
 then
-    pip install -U appdirs chardet configr termwidth coverage python-coveralls "typing >= '3.5' ; python_version < '3.5'"
+    pip install -U appdirs chardet configr termwidth coverage python-coveralls "typing >= '3.5' ; python_version < '3.5'" "enum34 ; python_version < '3.4'"
     # "setuptools >= 20.8.1" coconut-develop[mypy,cPyparsing,jobs]
 else
-    pip install -U appdirs chardet configr termwidth coverage python-coveralls "typing >= '3.5' ; python_version < '3.5'"
+    pip install -U appdirs chardet configr termwidth coverage python-coveralls "typing >= '3.5' ; python_version < '3.5'" "enum34 ; python_version < '3.4'"
     # "setuptools >= 20.8.1" coconut-develop[cPyparsing,jobs]
-fi
-
-if [ "x$BACKPORT" == "xtrue" ]
-then
-    pip install -U enum34
 fi
