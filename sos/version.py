151de3e32ed1b452808c9b55aadba06a6a424f47
ArneBachmann <ArneBachmann@users.noreply.github.com>
2018-07-03 18:34:20 +0200

    Fixes

--- sos/version.py
+++ sos/version.py
@@ -1,3 +1,3 @@
-__version_info__ = (2018, 1702, 3216)
-__version__ = r'2018.1702.3216-v1.6.0-48-g6d5a9b4'
+__version_info__ = (2018, 1703, 2822)
+__version__ = r'2018.1703.2822-v1.6.0-49-g32ee6d7'
 __release_version__ = '1.6.7'
