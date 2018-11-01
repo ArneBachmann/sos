bd9cb3c66addb5117f972554657e9d22c092b3aa
ArneBachmann <ArneBachmann@users.noreply.github.com>
2018-11-01 18:42:50 +0100

    Increased micro release number

--- sos/version.py
+++ sos/version.py
@@ -1,3 +1,3 @@
-__version_info__ = (2018, 2101, 2644)
-__version__ = r'2018.2101.2644-v1.6.0-62-gdcee386'
-__release_version__ = '1.6.8'
+__version_info__ = (2018, 2101, 2841)
+__version__ = r'2018.2101.2841-v1.6.0-63-ge87f4c1'
+__release_version__ = '1.6.9'
