e98aaa49df82e53a257ea40ddd90a0ee7217a916
ArneBachmann <ArneBachmann@users.noreply.github.com>
2018-12-04 16:47:21 +0100

    Improving test target platforms

--- install.bat
+++ install.bat
@@ -4,9 +4,9 @@ echo NOMYPY=%NOMYPY%
 python -m pip install -U pip
 
 if "%NOMYPY%" == "" (
-    pip install -U appdirs chardet configr termwidth "coverage == 4.0.3" python-coveralls "typing >= '3.5'; python_version < '3.5'" "enum34; python_version < '3.4'"
+    pip install -U appdirs chardet configr termwidth "coverage == 4.0.3" python-coveralls "typing >= '3.5'; python_version < '3.5'" "enum34; python_version < '3.4'" "setuptools == 30.1.0; python_version == '3.4'"
     rem "setuptools >= 20.8.1" coconut-develop[mypy,cPyparsing]
 ) else (
-    pip install -U appdirs chardet configr termwidth "coverage == 4.0.3" python-coveralls "typing >= '3.5'; python_version < '3.5'" "enum34; python_version < '3.4'"
+    pip install -U appdirs chardet configr termwidth "coverage == 4.0.3" python-coveralls "typing >= '3.5'; python_version < '3.5'" "enum34; python_version < '3.4'" "setuptools == 30.1.0; python_version == '3.4'"
     rem "setuptools >= 20.8.1" coconut-develop[cPyparsing]
 )
